8939039ff42ec11ac397e82655a54cfd867639e3
M-Huzaifa06 <mughalhuzaifa3486@gmail.com>
2026-04-10 16:56:18 +0500

    2nd commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-white p-4 sticky top-0">
+    <header className="bg-white p-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center w-[90vw]">
         <div className="text-2xl font-bold text-gray-800">MyShop</div>
         <nav className="hidden lg:flex items-center">
